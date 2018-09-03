956aacbd4dd9e714c6e7aee63dcb1cc104ff24c9
hyunjiyoo <solteee23@gmail.com>
2018-09-03 20:15:14 +0900

    router 미들웨어 사용

---
+++ hyunji/modern-web/chap21/code_21_17.js
@@ -0,0 +1,11 @@
+// 모듈을 추출합니다.
+var express = require('express');
+
+// 웹 서버를 생성합니다.
+var app = express();
+app.use(express.static('public'));
+
+// 웹 서버를 실행합니다.
+app.listen(52273, function () {
+    console.log('Server Running at http://127.0.0.1:52273');
+});
