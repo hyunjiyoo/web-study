6f4261e3db6c010881ab35c2d6c9cb50ea34a70d
hyunjiyoo <solteee23@gmail.com>
2019-01-16 20:42:25 +0900

    send() 메서드

---
+++ hyunji/node-web/chap08/code08_05.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(function (req, res) {
+    var output = [];
+    for(var i = 0; i < 3; i++) {
+        output.push({
+            count: i,
+            name: 'name - ' + i
+        });
+    }
+    res.send(output);
+});
+
+app.listen(52273, function () {
+    console.log('server running');
+});
