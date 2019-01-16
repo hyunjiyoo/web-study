47dad47f33b189aa619f815dd7a06e554d46a15e
hyunjiyoo <solteee23@gmail.com>
2019-01-16 11:48:05 +0900

    modele 생성후 사용

---
+++ hyunji/node-web/chap03/code03_07.js
@@ -0,0 +1,14 @@
+/* module.js 파일 */
+// 절대값을 구하는 메서드
+exports.abs = function (number) {
+    if (0 < number) {
+        return number;
+    } else {
+       return -number;
+    }
+};
+
+// 원의 넓이를 구하는 메서드입니다.
+exports.circleArea = function (radius) {
+    return radius * radius * Math.PI;
+};
