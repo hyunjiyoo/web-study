7bbe26f1d4144fe42cac61f719ec1c3f13046759
hyunjiyoo <solteee23@gmail.com>
2019-01-16 13:43:27 +0900

    setMaxListeners() 이벤트연결개수 조절

---
+++ hyunji/node-web/chap05/code05_06.js
@@ -0,0 +1,15 @@
+/* setMaxListener() 메서드 : 이벤트리스너 연결개수 조절 */
+process.setMaxListeners(15);
+
+process.on('exit', function () { });
+process.on('exit', function () { });
+process.on('exit', function () { });
+process.on('exit', function () { });
+process.on('exit', function () { });
+process.on('exit', function () { });
+process.on('exit', function () { });
+process.on('exit', function () { });
+process.on('exit', function () { });
+process.on('exit', function () { });
+process.on('exit', function () { });
+process.on('exit', function () { });
