eec8175a8747a6410ef4dad473cf6fde11001cd2
hyunjiyoo <solteee23@gmail.com>
2019-01-17 14:35:12 +0900

    mysql 모듈

---
+++ hyunji/node-web/chap09/code09_03.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+// 데이터베이스와 연결
+var client = mysql.createConnection({
+    user: 'root',
+    password: 'password',
+    database: 'Company2'
+});
+
+// 데이터베이스 쿼리 사용
+client.query('SELECT * FROM products', function (err, result, fields) {
+   if(err) {
+       console.log('쿼리 문장에 오류가 있습니다.');
+   } else {
+       console.log(result);
+   }
+});
